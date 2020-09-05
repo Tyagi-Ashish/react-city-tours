4a5053ef90cfd3eef997b05a6e50f5bed92663df
Tyagi-Ashish <marktyagi1234@gmail.com>
2020-09-06 00:33:52 +0530

    first commit

---
+++ src/tourData.js
@@ -0,0 +1,36 @@
+export const tourData = [
+
+  {
+    id: 1,
+    city: "new york",
+    img: "https://via.placeholder.com/100x70",
+    name: "new york bridge tour",
+    info: 
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, earum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, debitis."
+  },
+  {
+    id: 2,
+    city: "new york",
+    img: "https://via.placeholder.com/100x70",
+    name: "new york bridge tour",
+    info: 
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, earum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, debitis."
+  },
+  {
+    id: 3,
+    city: "new york",
+    img: "https://via.placeholder.com/100x70",
+    name: "new york bridge tour",
+    info: 
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, earum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, debitis."
+  },
+  {
+    id: 4,
+    city: "new york",
+    img: "https://via.placeholder.com/100x70",
+    name: "new york bridge tour",
+    info: 
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, earum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, debitis."
+  }
+
+]
